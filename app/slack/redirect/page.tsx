64bc5ac77c94471e73276371c16b607f1321a847
Arpit Sharma <arpit.sharma@whatfix.com>
2025-07-09 14:52:26 +0530

    Adds Slack redirect page
    Adds a basic page to inform users that they have successfully connected their Slack account.

---
+++ app/slack/redirect/page.tsx
@@ -0,0 +1,8 @@
+
+export default function SlackRedirect() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      You have successfully connected your Slack account.
+    </div>
+  );
+}
